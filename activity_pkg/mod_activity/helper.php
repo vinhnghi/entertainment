bc6aaa3d1399674cc58524a22fb46f013c5f6c80
Phi Tien <im.phitien@gmail.com>
2015-09-20 18:28:14 +0800

    add recent activity module

---
+++ activity_pkg/mod_activity/helper.php
@@ -0,0 +1,24 @@
+<?php
+defined ( '_JEXEC' ) or die ();
+//
+JLoader::register ( 'SiteActivityHelper', JPATH_SITE . '/components/com_activity/helpers/activity.php' );
+//
+class ModActivityHelper {
+	public static function getList($itemId, $parent_id, $limit) {
+		$query = SiteActivityHelper::getListActivitiesOfTypeQuery ( $parent_id );
+		$query->where ( 'a.published = 1' );
+		if ($parent_id)
+			$query->where ( 'c.published = 1' );
+		$query->order ( 'a.created DESC' );
+		$db = JFactory::getDbo ();
+		$db->setQuery ( $query, 0, $limit );
+		$items = $db->loadObjectList ();
+		
+		$key = "mod_activity.$itemId.$parent_id.$limit";
+		$cache = JFactory::getCache ( 'mod_activity', '' );
+		$cache->store ( $items, $key );
+		
+		return $items;
+	}
+}
+
